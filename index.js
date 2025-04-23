3d28581a0c7bf3434be3d3181e707c5e4dc5c56f
Rakhsrb <suhrobrahmatullayev973132@gmail.com>
2025-04-23 21:57:02 +0500

    port

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import BlogRoutes from "./routes/blog.js";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
